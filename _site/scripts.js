6609807089f15983b162ed5ccfe6eb2e612f7024
Natliejade <108981302+Natliejade@users.noreply.github.com>
2023-09-07 15:32:08 +0100

    mailerlite form JS added to work

--- _site/scripts.js
+++ _site/scripts.js
@@ -25,3 +25,11 @@ lightGallery(document.getElementById('lightgallery-2'), {
   speed: 500,
   download: false
 });
+
+//  MailerLite Universal 
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+.push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+ml('account', '123827');
+
